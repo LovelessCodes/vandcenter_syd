a760304b656945ae5e2903a33939d277dec129cf
Mike Kold Hermann <mkh@cleanmanager.dk>
2026-03-30 14:54:09 +0200

    feat: remove unused token import from const
    Remove unused CONF_TOKEN import from __init__.py and add aiohttp to
    requirements.txt

--- custom_components/vandcenter_syd/__init__.py
+++ custom_components/vandcenter_syd/__init__.py
@@ -3,7 +3,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN, CONF_TOKEN
+from .const import DOMAIN
 from .coordinator import VandCenterCoordinator
 
 PLATFORMS = [Platform.SENSOR]
