56019f945d4870748ddc41945f849b98e2aac4a9
Mike Kold Hermann <mkh@cleanmanager.dk>
2026-04-01 08:16:02 +0200

    feat: add coordinator support to VandCenterRefreshButton

--- custom_components/vandcenter_syd/button.py
+++ custom_components/vandcenter_syd/button.py
@@ -1,18 +1,19 @@
 """Buttons for VandCenter Syd."""
 
 from homeassistant.components.button import ButtonEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN
 
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
-    coordinator = hass.data[DOMAIN][config_entry.entry_id]
+    coordinator = hass.data[DOMAIN][config_entry.entry_id]["coordinator"]
     async_add_entities([VandCenterRefreshButton(coordinator)])
 
 
-class VandCenterRefreshButton(ButtonEntity):
+class VandCenterRefreshButton(CoordinatorEntity, ButtonEntity):
     def __init__(self, coordinator):
-        self.coordinator = coordinator
+        super().__init__(coordinator)
         self._attr_name = "Vandcenter Syd Refresh"
         self._attr_unique_id = "vandcenter_syd_refresh"
         self._attr_icon = "mdi:refresh"
